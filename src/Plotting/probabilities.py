f08c186f8a7f5ab371a58cbaad490e4b8b487ddf
Marta Estarellas <mpascualestarellas@gmail.com>
2018-03-02 01:30:08 +0000

    Fixed scripts plots

--- src/Plotting/probabilities.py
+++ src/Plotting/probabilities.py
@@ -1,31 +1,33 @@
-import sys
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from matplotlib.colors import colorConverter
 import numpy as np
 from pylab import *
 
-#eigenstates probabilities
-eigenstates=np.loadtxt('probabilities.data',skiprows=1)
+#READ DATA FROM FILE
+eigen=np.loadtxt('probabilities.data',skiprows=1)
 
-fig = plt.figure(figsize=(12,7))
+#SET SIZE FIGURE
+fig = plt.figure(figsize=(10,7))
 
 ax=plt.subplot()
 
-nvecs=int(sys.argv[1])
-
-i=list(range(1,nvecs))
-
-ax.set_xlim([1-(0.1*nvecs),nvecs+(0.1*nvecs)])
+#SET LIMITS
+ax.set_xlim([-1,len(eigen)])
 ax.set_ylim([0,1])
-ax.set_ylabel(r'$|c_{i,m}|^2$',fontsize=25)
+
+#SET LABEL NAMES
+ax.set_ylabel(r'${\cal{P}}_{i,k}$',fontsize=25)
+ax.set_xlabel('site number, $i$',fontsize=25)
+
+#SET SIZE OF THE AXIS TICKS
 ax.tick_params(axis='x', labelsize=20)
 ax.tick_params(axis='y', labelsize=20)
-ax.set_xlabel('|i>',fontsize=25)
-ax.plot(i,eigenstates)
 
+#PLOT
+ax.plot(eigen)
 
-plt.title('Eigenstate Site-Probabilities')
+fig.tight_layout()
 
-fig.set_tight_layout(True)
-savefig('probabilities.png',transparent=True)
+#SAVE FIGURE IN A PNG FILE
+savefig('eigenstates.png',transparent=True)
